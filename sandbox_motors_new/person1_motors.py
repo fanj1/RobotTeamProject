45f1d7928b6ad72fcf1adccc3e150214239a61ac
fanj1 <fanj@rose-hulman.edu>
2018-04-30 10:45:53 -0400

    m1

--- sandbox_motors_new/person1_motors.py
+++ sandbox_motors_new/person1_motors.py
@@ -44,6 +44,16 @@ def test_forward_backward():
         stop_action = str(input('stop action ("brake", "coast" or "hold"): '))
         forward_by_time(inches, speed, stop_action)
 
+    while True:
+        inches = int(input('distance to travel in inches (larger than 0): '))
+        if inches == 0:
+            break
+        speed = int(input('speed to travel (-100, 100): '))
+        stop_action = str(input('stop action ("brake", "coast" or "hold"): '))
+        forward_by_encoders(inches, speed, stop_action)
+
+
+
 
 def forward_seconds(seconds, speed, stop_action):
     """
@@ -82,7 +92,7 @@ def forward_by_time(inches, speed, stop_action):
     assert left_motor.connected
     assert right_motor.connected
 
-    seconds = int(inches * 75 / speed)
+    seconds = inches * 75 / speed
     left_motor.run_forever(speed_sp=speed)
     right_motor.run_forever(speed_sp=speed)
     time.sleep(seconds)
@@ -99,6 +109,18 @@ def forward_by_encoders(inches, speed, stop_action):
       2. Move until the computed number of degrees is reached.
     """
 
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    assert left_motor.connected
+    assert right_motor.connected
+
+    degrees = inches * 75 / speed
+    left_motor.run_to_rel_pos(position_sp=degrees, speed_sp=speed)
+    right_motor.run_to_rel_pos(position_sp=degrees, speed_sp=speed)
+    left_motor.stop(stop_action=stop_action)
+    right_motor.stop(stop_action=stop_action)
+    
 
 def backward_seconds(seconds, speed, stop_action):
     """ Calls forward_seconds with negative speeds to achieve backward motion. """
