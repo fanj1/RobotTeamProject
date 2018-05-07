7903013fb42409a7d883f19f9fc6cac61383d5b5
fanj1 <fanj@rose-hulman.edu>
2018-05-07 10:40:24 -0400

    m1

--- sandbox_digital_sensors_new/person1_digital_sensors.py
+++ sandbox_digital_sensors_new/person1_digital_sensors.py
@@ -7,11 +7,11 @@ Person 1: ev3.TouchSensor
 Person 2: ev3.Button
 Person 3: ev3.RemoteControl
 
-Authors: David Fisher, David Mutchler and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher, David Mutchler and JUN FAN.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # -----------------------------------------------------------------------------
-# TODO: 2.  WITH YOUR INSTRUCTOR, discuss the "big picture" of this project,
+# DONE: 2.  WITH YOUR INSTRUCTOR, discuss the "big picture" of this project,
 #           as described in the   _README_FIRST.txt   file.
 #
 # When your   ** ENTIRE TEAM ** understands that:
@@ -34,9 +34,9 @@ def main():
     """ Calls the   TEST   functions in this module. """
     # Uncomment these tests as you proceed through this module.
 
-    # run_test_touch_sensor()
-    # run_test_wait_for_press()
-    # run_test_show_images()
+    run_test_touch_sensor()
+    run_test_wait_for_press()
+    run_test_show_images()
 
 
 def run_test_touch_sensor():
@@ -105,8 +105,14 @@ def print_state_of_touch_sensor(n, seconds_per_print):
        1. Prints the STATE of the touch sensor.
        2. SLEEPs for the given number of seconds.
     """
+
+    for k in range(n):
+        a = ev3.TouchSensor.is_pressed()
+        print(a)
+        time.sleep(seconds_per_print)
+
     # -------------------------------------------------------------------------
-    # TODO: 3.  Implement and test this function.
+    # DONE: 3.  Implement and test this function.
     #           Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
@@ -150,8 +156,15 @@ def wait_for_press():
             (i.e., when the touch sensor is pressed).
        2. Sleeps for a small amount (say, 0.05 seconds).
     """
+
+    while True:
+        a = ev3.TouchSensor.is_pressed()
+        If a == 1:
+            break
+        time.sleep(0.05)
+
     # -------------------------------------------------------------------------
-    # TODO: 4.  Implement and test this function.
+    # DONE: 4.  Implement and test this function.
     #           Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
