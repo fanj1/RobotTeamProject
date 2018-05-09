af39f19fe6bde6fd27d2207ce9f9deebd856a65e
fanj1 <fanj@rose-hulman.edu>
2018-05-09 10:42:45 -0400

    m1

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -25,7 +25,7 @@ class Snatch3r(object):
         assert self.left_motor.connected
         assert self.right_motor.connected
 
-    def forward(self, inches, speed, stop_action='break'):
+    def forward(self, inches, speed, stop_action):
         degrees_motor = 90 * inches
         self.left_motor.run_to_rel_pos(position_sp=degrees_motor,
                                        spped_sp=speed,
@@ -37,7 +37,7 @@ class Snatch3r(object):
         self.left_motor.wait_while('running')
         self.right_motor.wait_while('running')
 
-        self.left_motor.stop()
-        self.right_motor.stop()
-        
+        self.left_motor.stop(stop_action=stop_action)
+        self.right_motor.stop(stop_action=stop_action)
+
 
